f8f0a928473b3638f95ebfd7458d96d4b31a0ef9
bnadeem-taknik <64201828+bnadeem-taknik@users.noreply.github.com>
2022-06-01 13:18:35 +0500

    Initial commits

---
+++ build/precache-manifest.858ee41069570c9b720d4d50c4d79723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773350e0e1a49bd844e289b79ecee4bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea981fff27b40705132",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d203c18cb56d8d6cddcc",
+    "url": "/static/js/2.83ec5540.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.83ec5540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea981fff27b40705132",
+    "url": "/static/js/main.ea21e640.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
